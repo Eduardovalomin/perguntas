d296454a687192c6cd1c4980d12e7988f47798f1
Eduardovalomin <134930934+Eduardovalomin@users.noreply.github.com>
2024-10-24 09:46:21 -0300

    Add files via upload

--- Flashcards-main/perguntas.js
+++ Flashcards-main/perguntas.js
@@ -1,43 +1,526 @@
+
+
 criaCartao(
-    'Astronomia',
-    'O que é gravidade?',
-    'De acordo com a relatividade geral, é a deformação do espaço tempo causado por um objeto massivo como uma estrela ou um planeta, por exemplo'
+    ' Moto',
+    ' Qual é a moto mais rápida do mundo?',
+    ' Kawasaki Ninja H2R (400 km/h)'
+)
+
+
+criaCartao(
+    'Moto',
+    'Qual é a moto mais querida do Brasil ?',
+    'Honda CG 160'
+)
+
+
+criaCartao(
+    'Carro',
+    'Qual é o carro mais potente do mundo ?',
+    'Bugatti Veyron Super Sport'
+)
+
+
+criaCartao(
+    'Filme',
+    'Qual é o carro do Toretto ?',
+    'Dodge Charger R / T'
+)
+
+
+criaCartao(
+    'Filme',
+    'Qual é o carro do brian ?',
+    'Nissan Skyline GT-R'
+)
+
+
+criaCartao(
+    'Moto',
+    'O que é freio ABS ? ',
+    'Sigla para Anti-lock Braking System (Sistema de Freios Antibloqueio)'
+)
+
+
+criaCartao(
+    'Moto',
+    'Como funciona os freios ABS ?',
+    'O sistema ABS ajusta automaticamente a pressão do freio para evitar o travamento das rodas, mantendo a tração e o controle do veículo.'
+)
+
+
+criaCartao(
+    'Moto',
+    'Qual é as vantagens do freio ABS',
+    'Segurança, Redução do desgaste dos pneus, Eficiência na frenagem'
+)
+'Qual é o ph da água pura?hifter ?',
+    'É um dispositivo utilizado em motocicletas que permite a troca de marchas sem a necessidade de usar a embreagem ou reduzir a aceleração'
+
+
+
+criaCartao(
+    'Moto',
+    'Qual é a vantagen de ter um quickshifter na moto ?',
+    'Desempenho, Conforto.'
 )
 
 criaCartao(
-    'Biologia',
-    'O que é câncer?',
-    'É o crescimento desordenado das células devido a alterações no mecanismo celular'
+    'Carro',
+    'Qual é a função do turbo',
+    'O turbo aumenta a potência e a eficiência do motor ao usar os gases de escape para comprimir o ar que entra nas câmaras de combustão. Isso resulta em uma combustão mais eficiente e maior desempenho.'
+)
+'Qual é o ph da água pura ?'
+
+criaCartao(
+    'Caminhão',
+    'Porque o caminhão paga mais caro no pedágio ?',
+    'Porque tem mais de 2 eixos'
+)
+
+criaCartao(
+    'Carro',
+    'Quantos escapamentos vem normalmente em um carro ?',
+    '  1 '
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da Austrália?',
-    'Camberra'
+    'Qual é o maior oceano do mundo ?',
+    'O Oceano Pacífico.'
 )
 
 criaCartao(
-    'Inglês',
-    'Como se diz mar em inglês?',
-    'Sea'
+    'Historia',
+    'Quem foi o primeiro homem a pisar na Lua ?',
+    'Neil Armstrong.'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil ?',
+    'Brasília.'
 )
 
 criaCartao(
-    'Matemática',
-    'Qual é a raiz quadrada de -4?',
-    'É mais ou menos 2i'
+    'Historia',
+    'Em que ano começou a Primeira Guerra Mundial ?',
+    'Em 1914'
 )
 
 criaCartao(
-    'Química',
-    'O que significa densidade?',
-    'É quanto determinada massa de um elemento ocupa certo espaço'
+    'Hitoria',
+    'Qando começou e terminou o regime militar no Brasil ?',
+    'O regime militar começou em 31 de março de 1964 e terminou em 15 de março de 1985'
 )
 
 criaCartao(
-    'Química',
-    'Qual é a moto mais rápida do mundo em 2024?'
+    'Historia',
+    'Qual foi um dos principais atos que aumentou a repressão durante o governo militar ?',
+    'O ato istitucional n°5 '
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o presidente deposto pelo golpe de 1964 ?',
+    'João Goulart'
+)
+
+criaCartao(
+    'historia',
+    'Em que ano ocorreu a trasição para um governo civil no Brasil ?',
+    'Abril de 1985'
+)
+
+criaCartao(
+    'Fisica',
+    'O que é a força  gravitacional ?',
+    'Atração emtre corpos devido a maça'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a unidade de medida da força no sistema internacional ?',
+    'Newton(N)'
+)
+
+criaCartao(
+    'Fisica',
+    'O que é energia potencial ?',
+    'Energia amarzenada devido a posição ou estado do objeto'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é o simbolo quimico do oxigenio ?',
+    'O'
+)
+
+criaCartao(
+    'Quimica',
+    'O que é uma reação quimica ?',
+    'Tranformação de subistancias atraves da reorganização dos atomos'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual é o ph da água pura ?',
+    '7(Neutro)'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são celulas procariontes ?',
+    'Celulas sem nucleo definido'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função dos ribrosomos nas celulas ?',
+    'Sintese de proteinas'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossintese ?',
+    'Processo que converte luz solar em energia quimica'
+)
+
+criaCartao(
+    'Robótica',
+    'O que é robótica e quais são suas principais áreas de aplicação ?',
+    'é um campo da engenharia e ciência que envolve a concepção, construção, operação e uso de robôs.'
+)
+
+criaCartao(
+    'Robótica',
+    'Quais são os tipos mais comuns de robôs utilizados na indústria ?',
+    'Os tipos mais comuns de robôs utilizados na indústria incluem Robôs industriais, Robôs colaborativos (cobots)'
+)
+
+criaCartao(
+    'Robótica',
+    'Como a inteligência artificial está integrada na robótica atual ?',
+    ' A inteligência artificial (IA) está integrada na robótica atual por meio do uso de algoritmos que permitem que os robôs aprendam com dados e experiências.'
+)
 
 
-    
+criaCartao(
+    'Robótica',
+    'Quais são os desafios éticos associados ao uso de robôs em ambientes de trabalho?',
+    'Substituição de empregos, Segurança, Privacidade'
+)
+
+
+criaCartao(
+    'Robótica',
+    'Como os robôs podem ajudar na medicina e na saúde ?',
+    'Cirurgias assistidas por robô, Reabilitação,Telemedicina, Desinfecção '
+)
+
+
+criaCartao(
+    'Robótica',
+    'O que são robôs autônomos e como eles funcionam?',
+    'Robôs autônomos são capazes de operar sem intervenção humana direta. Eles usam sensores para perceber o ambiente, algoritmos de IA para processar informações e tomar decisões baseadas em dados. Exemplos incluem drones e veículos autônomos. Eles geralmente têm sistemas de navegação que permitem mapear o ambiente e evitar obstáculos.'
+)
+
+
+criaCartao(
+    'Robótica',
+    'Quais são as diferenças entre robôs móveis e robôs fixos?',
+    'Robôs móveis: Podem se deslocar livremente em um ambiente, como AGVs ou drones. São projetados para navegar e interagir com diferentes locais, Robôs fixos: Geralmente estão estacionários, como braços robóticos em linhas de montagem. Eles executam tarefas específicas em um espaço definido sem se mover.'
+)
+
+criaCartao(
+    'Robótica',
+    'Como a programação de robôs é realizada?',
+    ' A programação de robôs pode ser feita através de linguagens específicas (como Python ou C++) ou softwares dedicados. Os programadores criam sequências de comandos que definem como o robô deve se comportar em diferentes situações. Além disso, métodos como programação por demonstração permitem que os operadores ensinem o robô observando-os executar uma tarefa.'
+)
+
+
+criaCartao(
+    'Robótica',
+    'O que é a robótica colaborativa e como ela se difere da robótica tradicional?',
+    'A robótica colaborativa envolve robôs que trabalham ao lado de humanos em um ambiente compartilhado. Eles são projetados para interagir de forma segura com pessoas, usando sensores que detectam a presença humana e adaptam seu comportamento para evitar acidentes. Isso difere da robótica tradicional, onde os robôs geralmente operam em áreas isoladas e têm tarefas fixas, sem interação direta com humanos.'
+)
+
+criaCartao(
+    'Robótica',
+    'Quais são as principais inovações recentes no campo da robótica?',
+    'Robôs autônomos,Robôs flexíveis,Integração com IoT  '
+)
+
+
+criaCartao(
+    'Robótica',
+    'Como a realidade aumentada pode ser aplicada na programação de robôs?',
+    ''
+)
+
+criaCartao(
+    'Robótica',
+    'Quais são os impactos da automação robótica no mercado de trabalho?',
+    'A realidade aumentada pode ser utilizada para criar interfaces visuais interativas que ajudam os programadores a visualizar o ambiente do robô e suas interações. Isso permite simulações em 3D antes da execução real, facilitando o ajuste de movimentos e comportamentos e melhorando a eficiência da programação.'
+)
+
+criaCartao(
+    'Robótica',
+    'O que é um braço robótico e como ele é utilizado em diferentes setores?',
+    'A automação pode levar à substituição de empregos em algumas áreas, mas também cria novas oportunidades em setores como desenvolvimento, manutenção e supervisão de robôs. Além disso, pode aumentar a produtividade e permitir que os trabalhadores se concentrem em tarefas mais complexas e criativas.'
+)
+
+
+criaCartao(
+    'Robótica',
+    'Como a tecnologia de sensores melhora a funcionalidade dos robôs?',
+    'Um braço robótico é um dispositivo mecânico que imita o movimento do braço humano'
+)
+
+criaCartao(
+    'Robótica',
+    'Quais são os principais componentes de um sistema robótico?',
+    ''
+)
+
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+criaCartao(
+    'Robótica',
+    '?',
+    ''
+)
+
+criaCartao(
+    'Robótica',
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
+)
+
+criaCartao(
+    '',
+    '?',
+    ''
 )
